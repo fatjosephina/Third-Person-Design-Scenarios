f298e54fc9bf7ecf459bf33d1fbb64afc7a44252
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2020-09-22 18:31:42 -0500

    Basic Movement with AddForce
    Added a player, a Rigidbody to the player, and got the player moving

---
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float accelerationForce = 10f;
+
+    private new Rigidbody rigidbody;
+    private Vector2 input;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDirection = new Vector3(input.x, 0, input.y);
+        rigidbody.AddForce(inputDirection * accelerationForce);
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+}
