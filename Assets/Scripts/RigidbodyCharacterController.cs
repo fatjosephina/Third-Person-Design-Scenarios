92fd6ae133e753601fcd9363d6011463428916b0
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2020-09-22 22:24:28 -0500

    Physics materials
    Added physics materials

--- Assets/Scripts/RigidbodyCharacterController.cs
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -10,20 +10,37 @@ public class RigidbodyCharacterController : MonoBehaviour
     [SerializeField]
     private float maxSpeed = 2f;
 
+    [SerializeField]
+    private PhysicMaterial stoppingPhysicMaterial, movingPhysicMaterial;
+
     private new Rigidbody rigidbody;
     private Vector2 input;
+    private new Collider collider;
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
     }
 
     private void FixedUpdate()
     {
         var inputDirection = new Vector3(input.x, 0, input.y);
+
+        collider.material = inputDirection.magnitude > 0 ? movingPhysicMaterial : stoppingPhysicMaterial;
+
+        /*if (inputDirection.magnitude > 0)
+        {
+            collider.material = movingPhysicMaterial;
+        }
+        else
+        {
+            collider.material = stoppingPhysicMaterial;
+        }*/
+
         if (rigidbody.velocity.magnitude < maxSpeed)
         {
-            rigidbody.AddForce(inputDirection * accelerationForce);
+            rigidbody.AddForce(inputDirection * accelerationForce, ForceMode.Acceleration);
         }
     }
 
